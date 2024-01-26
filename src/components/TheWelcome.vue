69430c779da53eb6dd05a2555ee89d964c93e209
Lucas Gabriel <lucmsilva651@gmail.com>
2024-01-26 20:40:12 -0300

    #19 | Added more strings (sorry for duplicate #17)

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -69,10 +69,8 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <SupportIcon />
     </template>
-    <template #heading>Support Vue</template>
+    <template #heading>Support me</template>
 
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    Support me following my social media and rating my work. I can't accept donations by now. But thanks for the visit.
   </WelcomeItem>
 </template>
