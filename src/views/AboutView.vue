fb18207020061a93aadde3bde97ec34d0587b07f
Lucas Gabriel <lucmsilva651@gmail.com>
2024-01-26 19:38:31 -0300

    #13 | Another update in About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="about">
+    <h1>About</h1>
     <h3>This project is made with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
       <br>
       Using icons and template from
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <br>
       Hosted and tested on
       <a href="https://www.vercel.com" target="_blank" rel="noopener">Vercel</a>.
     </h3>
