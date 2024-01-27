773877bae267301650b046bf63950c8024712a45
Lucas Gabriel <lucmsilva651@gmail.com>
2024-01-26 21:45:28 -0300

    #27 | Trying to fix build

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { followingDotCursor } from "cursor-effects";
-new followingDotCursor();
-
 const app = createApp(App);
 
 app.use(router)
