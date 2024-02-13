b94930f5ff0d7b5a9c2beeb76bf2308825062202
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-02-13 08:38:36 -0300

    Changed some strings and colors

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about">
-    <h3>This résumé is made with
+    <h3>
+      This résumé is made with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
       <br>
-      Using icons and template from
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      Using the <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a> template.
       <br>
       Hosted and tested on
       <a href="https://www.vercel.com" target="_blank" rel="noopener">Vercel</a>.
