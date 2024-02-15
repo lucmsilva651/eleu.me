64b014abd0b50dcbd60fde85550e7cafdd52d2fb
lucmsilva651@gmail.com <lucmsilva651@gmail.com>
2024-02-15 06:03:05 -0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 import { library } from '@fortawesome/fontawesome-svg-core'
+
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fas, far, fab)
+
 const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
