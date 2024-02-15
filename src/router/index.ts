553c57c6683751878c02aa2803c54c209d62b656
lucmsilva651@gmail.com <lucmsilva651@gmail.com>
2024-02-15 03:41:22 -0300

    update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PageNotFound from '.../views/PageNotFound.vue'
 
-export default router;
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
