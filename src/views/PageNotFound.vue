63838ac0bbb4ec6541b5eb4d771da435a79d1551
lucmsilva651@gmail.com <lucmsilva651@gmail.com>
2024-02-15 04:07:24 -0300

    update PageNotFound.vue

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -3,15 +3,27 @@ import WelcomeItem from '../components/WelcomeItem.vue'
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <font-awesome-icon icon="fa-solid fa-face-tired" />
-    </template>
-    <template #heading>404 Not Found</template>
-    
-    Whoops, it seems that this page was not found!
-    
-    You can try:
-    <RouterLink to="/">Go to Home</RouterLink>
-  </WelcomeItem>
+  <div class="notfound">
+    <WelcomeItem>
+      <template #icon>
+        <font-awesome-icon icon="fa-solid fa-face-tired" />
+      </template>
+      <template #heading>404 Not Found</template>
+      
+      Whoops, it seems that this page was not found!
+      
+      You can try:
+      <RouterLink to="/">Go to Home</RouterLink>
+    </WelcomeItem>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .notfound {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
