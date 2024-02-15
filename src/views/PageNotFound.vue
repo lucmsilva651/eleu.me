64ea0e7d622f2af16a95e0aafb285be2a8cca960
lucmsilva651@gmail.com <lucmsilva651@gmail.com>
2024-02-15 03:48:11 -0300

    update PageNotFound.vue

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="PageNotFound">
         Whoops, it seems that this page was not found!
+        You can try:
+        <RouterLink to="/">Go to Home</RouterLink>
     </div>
 </template>
