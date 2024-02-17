9ff07a4ef57e8bb7c8c9c3138dbbf180149f71ff
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-02-17 14:50:24 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <a href="https://forum.eleu.me" target="_blank" rel="noopener">
+          Forum
+        </a>
         <a href="https://github.com/lucmsilva651/eleu.me" target="_blank" rel="noopener">
           <font-awesome-icon icon="fa-brands fa-github" /> Source
         </a>
