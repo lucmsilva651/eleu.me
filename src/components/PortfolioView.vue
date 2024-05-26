a3185db14d3b157907e8d2b571c86a5a6c105d1f
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-05-25 21:08:25 -0300

    Website string and realocation update

--- src/components/PortfolioView.vue
+++ src/components/PortfolioView.vue
@@ -19,14 +19,14 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
     </template>
     <template #heading>What do I do?</template>
 
-    I create websites, scripts, digital art and a few other things.
+    I create websites, scripts, compilations and a few other things.
   </HomeItem>
 
   <HomeItem>
     <template #icon>
       <font-awesome-icon icon="fa-solid fa-toolbox" />
     </template>
-    <template #heading>What can I work with?</template>
+    <template #heading>What do I know how to work with?</template>
 
     <a href="https://vuejs.org/" target="_blank" rel="noopener">
       <font-awesome-icon icon="fa-brands fa-vuejs" /> Vue
@@ -43,21 +43,29 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
     <a href="https://developer.mozilla.org/docs/Web/JavaScript" target="_blank" rel="noopener">
       <font-awesome-icon icon="fa-brands fa-js" /> JavaScript
     </a>
-    
-    <a href="https://en.wikipedia.org/wiki/Batch_file" target="_blank" rel="noopener">
-      <font-awesome-icon icon="fa-solid fa-terminal" /> Batchfile
-    </a>
 
     <a href="https://www.python.org/" target="_blank" rel="noopener">
       <font-awesome-icon icon="fa-brands fa-python" /> Python
     </a>
     
-    <a href="https://gdevelop.io/" target="_blank" rel="noopener">
-      <font-awesome-icon icon="fa-solid fa-g" /> GDevelop
+    <a href="https://en.wikipedia.org/wiki/Batch_file" target="_blank" rel="noopener">
+      <font-awesome-icon icon="fa-solid fa-terminal" /> Batchfile
+    </a>
+
+    <a href="https://nodejs.org/en" target="_blank" rel="noopener">
+      <font-awesome-icon icon="fa-brands fa-node" /> Node.js
     </a>
     
     <a href="https://git-scm.com/" target="_blank" rel="noopener">
       <font-awesome-icon icon="fa-brands fa-git-alt" /> Git
+    </a>
+
+    <a href="https://www.npmjs.com/" target="_blank" rel="noopener">
+      <font-awesome-icon icon="fa-brands fa-npm" /> npm
+    </a>
+
+    <a href="https://www.microsoft.com/windows" target="_blank" rel="noopener">
+      <font-awesome-icon icon="fa-brands fa-windows" /> Windows
     </a>
   </HomeItem>
 
@@ -70,7 +78,19 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
     <a href="https://github.com/lucmsilva651/" target="_blank" rel="noopener">
       <font-awesome-icon icon="fa-brands fa-github" /> GitHub
     </a>
-    
+
+    <a href="https://youtube.com/lucmsilva651/" target="_blank" rel="noopener">
+      <font-awesome-icon icon="fa-brands fa-youtube" /> YouTube
+    </a>
+
+    <a href="https://last.fm/user/lucmsilva/" target="_blank" rel="noopener">
+      <font-awesome-icon icon="fa-brands fa-lastfm" /> Last.fm
+    </a>
+
+    <a href="https://tiktok.com/@lucmsilva/" target="_blank" rel="noopener">
+      <font-awesome-icon icon="fa-brands fa-tiktok" /> TikTok
+    </a>
+
     <a href="https://steamcommunity.com/id/lucmsilva/" target="_blank" rel="noopener">
       <font-awesome-icon icon="fa-brands fa-steam" /> Steam
     </a>
@@ -79,20 +99,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
       <font-awesome-icon icon="fa-brands fa-twitter" /> Twitter
     </a>
 
-    <a href="https://last.fm/user/lucmsilva/" target="_blank" rel="noopener">
-      <font-awesome-icon icon="fa-brands fa-lastfm" /> Last.fm
-    </a>
-
-    <a href="https://youtube.com/lucmsilva651/" target="_blank" rel="noopener">
-      <font-awesome-icon icon="fa-brands fa-youtube" /> YouTube
-    </a>
-
     <a href="https://facebook.com/lucmsilva/" target="_blank" rel="noopener">
       <font-awesome-icon icon="fa-brands fa-facebook" /> Facebook
-    </a>
-
-    <a href="https://tiktok.com/@lucmsilva/" target="_blank" rel="noopener">
-      <font-awesome-icon icon="fa-brands fa-tiktok" /> TikTok
     </a>
 
     <a href="https://instagram.com/lucasgabm95/" target="_blank" rel="noopener">
