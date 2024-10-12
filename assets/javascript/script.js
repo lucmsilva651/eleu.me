206e0633efe4de6962b63200d1ccdd6daca9f41b
Lucas Gabriel <lucmsilva651@gmail.com>
2024-10-12 13:38:21 -0300

    Design fixes + some things

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,10 +20,10 @@ const mIcon5 = document.getElementById("section-ico-5");
 const contentBox = document.getElementById("content-box");
 
 const tabContents = {
-  "about-me": `<h1 class="section-title">About me</h1>Hi! I'm Lucas, a 15 year old passionate programmer from Brazil. Welcome to my personal website!`,
-  "my-projects": "Here are some of my personal projects. Explore my <a href='https://github.com/lucmsilva651' target='_blank'>GitHub</a> for more!",
-  "friends": "These are my friends and collaborators who inspire me every day.",
-  "support-me": "If you'd like to support my work, consider donating or following me on social media!"
+  "about-me": `<h1 class="section-title">About me</h1><p>Hi! I'm Lucas, a 15 year old passionate programmer from Brazil. Welcome to my website!</p><p>Test</p>`,
+  "my-projects": `<h1 class="section-title">My projects</h1><p>Here are some of my personal projects. Explore my <a href='https://github.com/lucmsilva651' target='_blank'>GitHub</a> for more!</p>`,
+  "friends": `<h1 class="section-title">Friends</h1><p>These are my friends and collaborators who inspire me every day.<br>(sorry if I have forgot someone)</p><ul><li><a href="https://github.com/GiovaniFZ">Giovani Finazzi (aka. Giv)</li><li><a href="https://github.com/skye-pa1n">Skye</li><li><a href="https://github.com/VegaBobo">VegaBobo/VegaData (aka. Vega)</li><li><a href="https://github.com/oByteszOficial">Daniel (aka. Byt3z)</a></ul>`,
+  "support-me": `<h1 class="section-title">Support me</h1><p>If you'd like to support my work, consider donating or following me on social media!</p>`
 };
 
 function activateTab(tabId, iconId) {
@@ -45,35 +45,38 @@ function activateTab(tabId, iconId) {
   
   const sanitizedContent = DOMPurify.sanitize(tabContents[tabId]);
   contentBox.innerHTML = sanitizedContent;
-};
+
+  const url = `?tab=${tabId}`;
+  history.pushState({ tabId }, '', url);
+}
 
 function addHoverEffect(tab, icon) {
   tab.addEventListener('mouseenter', function() {
     if (!tab.classList.contains('active')) {
       tab.style.color = '#059ad3';
       icon.style.color = '#059ad3';
-    };
+    }
   });
 
   tab.addEventListener('mouseleave', function() {
     if (!tab.classList.contains('active')) {
       tab.style.color = '#445156';
       icon.style.color = '#445156';
-    };
+    }
   });
-};
+}
 
 aboutMeBtn.addEventListener('click', () => activateTab('about-me', 'section-ico-1'));
 myProjectsBtn.addEventListener('click', () => activateTab('my-projects', 'section-ico-2'));
 friendsBtn.addEventListener('click', () => activateTab('friends', 'section-ico-3'));
-bioBtn.addEventListener('click', () => location.href ="https://bio.eleu.me");
-supportBtn.addEventListener('click', () => activateTab('support-me', 'section-ico-5'));
+supportBtn.addEventListener('click', () => activateTab('support-me', 'section-ico-4'));
+bioBtn.addEventListener('click', () => window.open("https://bio.eleu.me", '_blank'));
 
 addHoverEffect(aboutMeBtn, mIcon1);
 addHoverEffect(myProjectsBtn, mIcon2);
 addHoverEffect(friendsBtn, mIcon3);
-addHoverEffect(bioBtn, mIcon4);
-addHoverEffect(supportBtn, mIcon5);
+addHoverEffect(supportBtn, mIcon4);
+addHoverEffect(bioBtn, mIcon5);
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -85,6 +88,16 @@ if (randomValue == 40028922) {
   document.getElementById("logo").style.fontFamily = "Comic Sans MS";
 }
 
+window.addEventListener('popstate', (event) => {
+  if (event.state && event.state.tabId) {
+    activateTab(event.state.tabId, `section-ico-${Object.keys(tabContents).indexOf(event.state.tabId) + 1}`);
+  } else {
+    activateTab('about-me', 'section-ico-1'); 
+  }
+});
+
 window.onload = () => {
-  activateTab('about-me', 'section-ico-1');
+  const params = new URLSearchParams(window.location.search);
+  const tabFromUrl = params.get('tab') || 'about-me';
+  activateTab(tabFromUrl, `section-ico-${Object.keys(tabContents).indexOf(tabFromUrl) + 1}`);
 };
