d8242024fe5507a56883f280017091e42479a494
Luquinhas <lucmsilva651@gmail.com>
2025-09-03 01:19:12 -0300

    Remove alert bar

--- js/index.js
+++ js/index.js
@@ -2,16 +2,7 @@ import { $, onDOMLoad } from "./utils/handleElem.min.js";
 import loadHTML from "./utils/loadHTML.min.js";
 import myAge from "./utils/myAge.min.js";
 
-function checkAlert() {
-  if (localStorage.getItem("isABClosed") == "true") {
-    $("alertBar").style.display = "none";
-  } else {
-    $("alertBar").style.display = "flex";
-  };
-};
-
 onDOMLoad(() => {
-  loadHTML("alert", "/pages/alert-bar.html");
   loadHTML("header", "/pages/header.html");
   loadHTML("home", "/pages/home-section.html");
   loadHTML("experience", "/pages/experience.html");
@@ -22,13 +13,5 @@ onDOMLoad(() => {
   
   setTimeout(() => {
     $("myAge").textContent = myAge("2009-07-07");
-    checkAlert();
   }, 500);
 });
-
-setTimeout(() => {
-  $("closeAlert").addEventListener("click", () => {
-    localStorage.setItem("isABClosed", "true");
-    checkAlert();
-  });
-}, 500);
