2c4cef0bd3db7818839d5cba62cd0ade7b131b1a
Luquinhas <lucmsilva651@gmail.com>
2025-10-21 15:11:52 -0300

    Change footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,21 @@
+<script setup>
+const year = new Date().getFullYear();
+</script>
+
 <template>
   <footer>
-    <p>&copy; Lucas Gabriel (lucmsilva). Using FontAwesome and SimpleIcons.</p>
+    <p>&copy; {{ year }} <a href="https://github.com/lucmsilva651" target="_blank" rel="noopener noreferrer">Lucas Gabriel (lucmsilva)</a>.</p>
+    <p>Website developed with <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">VueJS</a> and <a href="https://vite.dev/" target="_blank" rel="noopener noreferrer">Vite.</a></p>
+    <p>Icons by <a href="https://www.fontawesome.com" target="_blank" rel="noopener noreferrer">FontAwesome</a> and <a href="http://simpleicons.org" target="_blank" rel="noopener noreferrer">Simple Icons</a> via <a href="https://oh-vue-icons.js.org/" target="_blank" rel="noopener noreferrer">Oh, Vue Icons!</a>.</p>
   </footer>
 </template>
 
 <style scoped>
 footer {
+  line-height: 1.6;
   border-top: solid 1px #1d1d25;
   background-color: #24242e;
   text-align: center;
-  padding: 10px 60px;
+  padding: 15px 60px;
 }
 </style>
