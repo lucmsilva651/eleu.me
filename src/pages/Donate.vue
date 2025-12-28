20bd769ba5738169ea74071b85cf80889504f824
Luquinhas <lucmsilva651@gmail.com>
2025-12-28 01:08:45 -0300

    Refactor Donate.vue to simplify Pix donation details

--- src/pages/Donate.vue
+++ src/pages/Donate.vue
@@ -3,12 +3,7 @@
     <h1>Donate to me</h1>
     <p>Only donate if you actually don't need the money, I will not do refunds.</p>
     <br>
-    <details>
-      <summary>Pix (Brazil / BRL)</summary>
-      <blockquote>
-        <p>Email: <a href="mailto:lucmsilva651@gmail.com"><code>lucmsilva651@gmail.com</code></a></p>
-        <p>Random key: <code>8d6a266f-59e5-4084-be71-ee134b21e6de</code></p>
-      </blockquote>
-    </details>
+    <h3>Pix (Brazil / BRL)</h3>
+    <p>Email: <a href="mailto:lucmsilva651@gmail.com"><code>lucmsilva651@gmail.com</code></a></p>
   </section>
 </template>
