ddc2387ad459b340b2de75517808da402019e79d
Lucas Gabriel <lucmsilva651@gmail.com>
2026-02-24 22:00:25 -0300

    Add vue-router for navigation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import router from './router.js';
 import App from './App.vue';
 
 import './css/index.css';
@@ -7,3 +8,4 @@ import './css/responsivity.css';
 
 const app = createApp(App);
 app.mount('#app');
+app.use(router);
